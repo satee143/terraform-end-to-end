30fb22cab4faf5fd2e37a561eea744dc9b29cece
Satheesh Doosa <satee143@gmail.com>
2025-05-17 01:24:48 +0530

    removed application

--- main.tf
+++ main.tf
@@ -78,7 +78,7 @@ resource "aws_route" "public-route" {
 }
 
 resource "aws_route" "private-route" {
-  route_table_id         = aws_route_table.public-route-table.id
+  route_table_id         = aws_route_table.private-route-table.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_nat_gateway.example.id
 }
