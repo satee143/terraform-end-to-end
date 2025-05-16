262de8db5295376153d34d1933155eb038dcedec
Satheesh Doosa <satee143@gmail.com>
2025-05-17 01:31:53 +0530

    removed application

--- main.tf
+++ main.tf
@@ -71,6 +71,16 @@ resource "aws_route_table" "private-route-table" {
     })
 }
 
+resource "aws_route_table" "database-route-table" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(var.common_tags,
+    {
+      Name = "${local.resource_name}-database-route-table"
+
+    })
+}
+
 resource "aws_route" "public-route" {
   route_table_id         = aws_route_table.public-route-table.id
   destination_cidr_block = "0.0.0.0/0"
@@ -79,6 +89,12 @@ resource "aws_route" "public-route" {
 
 resource "aws_route" "private-route" {
   route_table_id         = aws_route_table.private-route-table.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_nat_gateway.example.id
+}
+
+resource "aws_route" "database-route" {
+  route_table_id         = aws_route_table.database-route-table.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_nat_gateway.example.id
 }
