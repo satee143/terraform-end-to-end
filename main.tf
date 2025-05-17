3dc7bcb50a6123d5bedbca1bbe9869952f919f3e
Satheesh Doosa <satee143@gmail.com>
2025-05-17 23:03:53 +0530

    Added code

--- main.tf
+++ main.tf
@@ -35,7 +35,7 @@ resource "aws_subnet" "private-subnets" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = var.private_subnet_cidrs[count.index]
   availability_zone       = local.az_names[count.index]
-  map_public_ip_on_launch = true
+  map_public_ip_on_launch = false
   tags = merge(var.common_tags, var.private_subnet_tags,
     { Name = "${local.resource_name}-private-${local.az_names[count.index]}" })
 }
@@ -45,11 +45,10 @@ resource "aws_subnet" "database-subnets" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = var.database_subnet_cidrs[count.index]
   availability_zone       = local.az_names[count.index]
-  map_public_ip_on_launch = true
+  map_public_ip_on_launch = false
   tags = merge(var.common_tags, var.database_subnet_tags,
     { Name = "${local.resource_name}-database-${local.az_names[count.index]}" })
 }
-
 
 resource "aws_route_table" "public-route-table" {
   vpc_id = aws_vpc.main.id
@@ -71,15 +70,15 @@ resource "aws_route_table" "private-route-table" {
     })
 }
 
-resource "aws_route_table" "database-route-table" {
-  vpc_id = aws_vpc.main.id
-
-  tags = merge(var.common_tags,
-    {
-      Name = "${local.resource_name}-database-route-table"
-
-    })
-}
+# resource "aws_route_table" "database-route-table" {
+#   vpc_id = aws_vpc.main.id
+#
+#   tags = merge(var.common_tags,
+#     {
+#       Name = "${local.resource_name}-database-route-table"
+#
+#     })
+# }
 
 resource "aws_route" "public-route" {
   route_table_id         = aws_route_table.public-route-table.id
@@ -93,11 +92,11 @@ resource "aws_route" "private-route" {
   gateway_id             = aws_nat_gateway.example.id
 }
 
-resource "aws_route" "database-route" {
-  route_table_id         = aws_route_table.database-route-table.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_nat_gateway.example.id
-}
+# resource "aws_route" "database-route" {
+#   route_table_id         = aws_route_table.database-route-table.id
+#   destination_cidr_block = "0.0.0.0/0"
+#   gateway_id             = aws_nat_gateway.example.id
+# }
 
 resource "aws_route_table_association" "public" {
   count = length(var.public_subnet_cidrs)
@@ -129,7 +128,6 @@ resource "aws_eip" "eip" {
 resource "aws_nat_gateway" "example" {
   allocation_id = aws_eip.eip.id
   subnet_id     = aws_subnet.public-subnets[0].id
-
   tags = merge(
     var.common_tags,
     {
