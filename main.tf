dbb19fcd4b89860de5f15b2fac50014a5f779060
Satheesh Doosa <satee143@gmail.com>
2025-05-20 23:49:27 +0530

    Added code related RDS

--- main.tf
+++ main.tf
@@ -1,11 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "5.81.0"
-    }
-  }
-}
 resource "aws_vpc" "main" {
   cidr_block           = var.vpc_cidr_block
   enable_dns_hostnames = var.enable_dns_hostnames
